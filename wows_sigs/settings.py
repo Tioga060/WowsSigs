e051e0f8883bf769736cf97ab4c4107b9eac7919
Tioga060 <jeffhanford060@gmail.com>
2017-04-22 00:25:42 -0700

    t

--- wows_sigs/settings.py
+++ wows_sigs/settings.py
@@ -1,5 +1,5 @@
 # Django settings for wows_sigs project.
-
+from mongoengine import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,14 +11,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'signatures',
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+        'HOST': 'localhost',
+        'PORT': '27017',
+        'SUPPORTS_TRANSACTIONS': False,
+    },
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
