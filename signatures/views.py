1d97760c0ba35a0a4aea4d28c97a058191d6df89
Tioga060 <jeffhanford060@gmail.com>
2017-04-22 01:45:07 -0700

    syntax fix

--- signatures/views.py
+++ signatures/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.conf import settings as djangoSettings
-from signatures import *
+import signatures
 from models import *
 
 def testview(request):
