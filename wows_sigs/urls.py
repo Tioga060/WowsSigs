6900f34e9fb92b39d9e4dfa5a60356912fbe77c3
Tioga060 <jeffhanford060@gmail.com>
2017-04-22 06:56:13 +0000

    recreating everyting

---
+++ wows_sigs/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'wows_sigs.views.home', name='home'),
+    # url(r'^wows_sigs/', include('wows_sigs.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
