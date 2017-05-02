2652e43f9eee0b69c08b0d724171620d5ffd39b0
Tioga060 <jeffhanford060@gmail.com>
2017-05-02 02:28:47 -0700

    test

--- signatures/urls.py
+++ signatures/urls.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.template import RequestContext
+
 from django.conf.urls import url,include
 from django.conf import settings
 from django.conf.urls.static import static
--- signatures/views.py
+++ signatures/views.py
@@ -1,10 +1,14 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings as djangoSettings
 from serializers import *
 from models import *
 from forms import SignatureForm
 from django.views.generic import TemplateView
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 def index(request):
     #p = Player(username="Jeff",playerid="123",signature={'test':False})
