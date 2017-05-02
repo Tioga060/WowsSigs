3ab6ba4b41cb2616050f39ef418c15d6674384f7
Tioga060 <jeffhanford060@gmail.com>
2017-05-01 19:36:31 -0700

    fix serializers

--- signatures/serializers.py
+++ signatures/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
-    inputs = ToolInputSerializer(many=True)
     class Meta:
         model = Player
         fields = '__all__'
