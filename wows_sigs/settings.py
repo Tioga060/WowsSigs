df2e03ac23086df6751f91b27299edebeca398f4
Tioga060 <jeffhanford060@gmail.com>
2017-05-02 01:43:08 -0700

    crispy forms install

--- wows_sigs/settings.py
+++ wows_sigs/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework_mongoengine',
     'djangular',
+    'crispy_forms',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
