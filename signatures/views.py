b564a52e2e09504207477f1219d2e3b6c007a884
Tioga060 <jeffhanford060@gmail.com>
2017-05-02 01:38:36 -0700

    test

--- signatures/views.py
+++ signatures/views.py
@@ -4,6 +4,7 @@ from django.conf import settings as djangoSettings
 from serializers import *
 from models import *
 from forms import SignatureForm
+from django.views.generic import TemplateView
 
 def testview(request):
     p = Player(username="Jeff",playerid="123",signature={'test':False})
