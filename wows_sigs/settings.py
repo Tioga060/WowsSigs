4d662b77cebcfe67c4c47efa6832c4b1912a9175
Tioga060 <jeffhanford060@gmail.com>
2017-05-02 02:11:34 -0700

    test

--- wows_sigs/settings.py
+++ wows_sigs/settings.py
@@ -67,11 +67,10 @@ STATIC_ROOT = '/root/wows_sigs/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = r'http://162.243.239.164:8080/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    APP_ROOT+"static/",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
