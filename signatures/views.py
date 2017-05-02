048d3b807b42b727d3bf5d670617e5ef6fcc01da
Tioga060 <jeffhanford060@gmail.com>
2017-05-02 01:35:49 -0700

    fix forms

--- signatures/views.py
+++ signatures/views.py
@@ -3,9 +3,20 @@ from django.http import HttpResponse
 from django.conf import settings as djangoSettings
 from serializers import *
 from models import *
-
+from forms import JobForm
 
 def testview(request):
     p = Player(username="Jeff",playerid="123",signature={'test':False})
     p.save()
     return HttpResponse("<h1>Resized Gif</h1>")
+
+
+
+
+class SignatureFormView(TemplateView):
+    template_name = "signatures/new.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SignatureFormView, self).get_context_data(**kwargs)
+        context.update(SignatureForm=SignatureForm())
+        return context
