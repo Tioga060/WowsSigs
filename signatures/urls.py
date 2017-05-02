441bd60644ed638378208b11bb6d97b3f9d48160
Tioga060 <jeffhanford060@gmail.com>
2017-05-02 01:24:07 -0700

    import fix

--- signatures/urls.py
+++ signatures/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 from viewsets import *
 from . import views
 
