a9deae81537f1d65bcc1a3cbeff5f3b8de82a47a
Tioga060 <jeffhanford060@gmail.com>
2017-05-04 11:28:36 -0700

    test

--- public/js/services/AuthService.js
+++ public/js/services/AuthService.js
@@ -10,6 +10,7 @@ angular.module('AuthService', []).service('Auth',['$http','$q', function($http,$
 		else {
 			$http.get('/signatures/api/cookie/?format=json').then(function(res){
 			  // set the result to a field on the service
+        console.log(res.data)
 			  if(res.data.username){
 				  me.currentUser = res.data;
 				  // call the callback with the retrieved user
