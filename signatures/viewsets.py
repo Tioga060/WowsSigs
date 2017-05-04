a6967d80cf94ea23e90d64ea9093c499dfdf301b
Tioga060 <jeffhanford060@gmail.com>
2017-05-04 11:02:34 -0700

    test new

--- signatures/viewsets.py
+++ signatures/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, response
 from models import *
 from serializers import *
+import django.shortcuts
 class PlayerViewSet(viewsets.ModelViewSet):
     '''
     Contains information about a command-line Unix program.
@@ -20,11 +21,11 @@ class PlayerCookieViewSet(viewsets.ModelViewSet):
     '''
     def list(self, request):
         queryset = Player.objects.all()
-        serializer = UserSerializer(queryset, many=True)
+        serializer = PlayerSerializer(queryset, many=True)
         return response.Response(serializer.data)
 
     def retrieve(self, request, pk='cookie'):
         queryset = Player.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
+        user = django.shortcuts.get_object_or_404(queryset, pk=pk)
         serializer = PlayerSerializer(user)
         return response.Response(serializer.data)
