6c4b8a119a63569081f35e62da426d31b3d323ff
Tioga060 <jeffhanford060@gmail.com>
2017-05-04 06:00:16 -0700

    fix sourcing, move public files

--- templates/js/app.js
+++
@@ -1,6 +0,0 @@
-// public/js/app.js
-angular.module('wows_sigs', ['ngRoute', 'ngCookies','appRoutes','ngFileUpload',
-  'MainCtrl',  'PlayerCtrl', 'HomeCtrl',
-  'AuthService', 'PlayerService',
-  //'angular-loading-bar', 'colorpicker.module','mgcrea.bootstrap.affix','ngNotificationsBar', 'ngSanitize'
-  ]);
--- templates/js/controllers/HomeCtrl.js
+++
@@ -1,9 +0,0 @@
-// public/js/controllers/HomeCtrl.js
-
-
-angular.module('HomeCtrl', []).controller('HomeController', ['$scope','$routeParams', 'Auth', function($scope, $routeParams, Auth) {
-
-	$scope.tagline = 'Tioga.moe Rules!';
-	//Auth.getUser(function(user){$scope.currentUser = user;});
-
-}]);
