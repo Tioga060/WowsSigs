4653559340f199d40c554bb5cf6c29eafa70fac3
Tioga060 <jeffhanford060@gmail.com>
2017-05-04 05:53:51 -0700

    add djing

--- wows_sigs/settings.py
+++ wows_sigs/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'rest_framework_mongoengine',
     'djangular',
     'crispy_forms',
+    'djng',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
